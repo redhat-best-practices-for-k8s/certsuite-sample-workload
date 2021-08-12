7604d7e4931c92f629a138532eb0109107a0e276
edcdavid <86730676+edcdavid@users.noreply.github.com>
2021-08-11 23:48:52 -0400

    Do not deploy fsdiff pod with Minikube (#28)

--- deploy-all.sh
+++ deploy-all.sh
@@ -14,7 +14,9 @@ else
 fi
 
 export res=$(oc get pod -n $NAMESPACE_TO_GENERATE -l test-network-function.com/generic=fs_diff_master 2>&1)
-if [[ ${res#No resources found} != ${res} ]]; then
+if [[ -n "${TNF_MINIKUBE_ONLY}" ]] && [[ ${TNF_MINIKUBE_ONLY} = "true" ]]; then
+    echo "Running with Minikube, no FsDiff pod created"
+elif [[ ${res#No resources found} != ${res} ]]; then
     cat ./local-test-infra/fsdiff-pod.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
     oc apply -f ./temp/rendered-fsdiff-template.yaml
     rm ./temp/rendered-fsdiff-template.yaml
