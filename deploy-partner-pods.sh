146a75e867a793808732bb3479b4871e18ee640f
Isaac Dorfman <idorfman@redhat.com>
2021-08-18 21:34:37 +0300

    CNFCERT-30 updated scripts to always update deployments

--- deploy-partner-pods.sh
+++ deploy-partner-pods.sh
@@ -16,25 +16,16 @@ fi
 if [[ -n "${TNF_MINIKUBE_ONLY}" ]] && [[ ${TNF_MINIKUBE_ONLY} = "true" ]]; then
     echo "Running with Minikube, no FsDiff pod created"
 else
-    export res=$(oc get deployment -n $NAMESPACE_TO_GENERATE node-master 2>&1)
-    if [[ ${res#Error from server (NotFound)} != ${res} ]] || [[ ${res#No resources found} != ${res} ]]; then
-        cat ./local-test-infra/fsdiff-deployment.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
-        oc apply -f ./temp/rendered-fsdiff-template.yaml
-        rm ./temp/rendered-fsdiff-template.yaml
-    else
-        echo "fsDiffMasterPod already exists, no reason to recreate"
-    fi
+    cat ./local-test-infra/fsdiff-deployment.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
+    oc apply -f ./temp/rendered-fsdiff-template.yaml
+    rm ./temp/rendered-fsdiff-template.yaml
     sleep 3
     oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=node-master" --timeout=30s
 fi
 
-export res=$(oc get deployment -n $NAMESPACE_TO_GENERATE partner 2>&1)
-if [[ ${res#Error from server (NotFound)} != ${res} ]] || [[ ${res#No resources found} != ${res} ]]; then
-    cat ./local-test-infra/local-partner-deployment.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
-    oc apply -f ./temp/rendered-partner-template.yaml
-    rm ./temp/rendered-partner-template.yaml
-else
-    echo "partner pod already exists, no reason to recreate"
-fi
+
+cat ./local-test-infra/local-partner-deployment.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
+oc apply -f ./temp/rendered-partner-template.yaml
+rm ./temp/rendered-partner-template.yaml
 sleep 3
 oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=partner" --timeout=30s
--- deploy-test-pods.sh
+++ deploy-test-pods.sh
@@ -4,13 +4,9 @@ if [[ -z "${NAMESPACE_TO_GENERATE}" ]]; then
     export NAMESPACE_TO_GENERATE="tnf"
 fi
 
-export res=$(oc get deployment -n $NAMESPACE_TO_GENERATE test 2>&1)
-if [[ ${res#Error from server (NotFound)} != ${res} ]] || [[ ${res#No resources found} != ${res} ]]; then
-    cat ./local-test-infra/local-pod-under-test.yaml | ./script/mo > ./temp/rendered-local-pod-under-test-template.yaml
-    oc apply -f ./temp/rendered-local-pod-under-test-template.yaml
-    rm ./temp/rendered-local-pod-under-test-template.yaml
-else
-    echo "target pod already exists, no reason to recreate"
-fi
+
+cat ./local-test-infra/local-pod-under-test.yaml | ./script/mo > ./temp/rendered-local-pod-under-test-template.yaml
+oc apply -f ./temp/rendered-local-pod-under-test-template.yaml
+rm ./temp/rendered-local-pod-under-test-template.yaml
 sleep 3
 oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=test" --timeout=30s
