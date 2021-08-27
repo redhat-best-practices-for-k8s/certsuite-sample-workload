2006d551f9d385d58e73005a02de70b9ed4d5d81
salah hamadi <shamadi@redhat.com>
2021-08-26 22:21:09 -0400

    fix it

--- deploy-partner-pods.sh
+++ deploy-partner-pods.sh
@@ -16,9 +16,9 @@ fi
 if [[ -n "${TNF_MINIKUBE_ONLY}" ]] && [[ ${TNF_MINIKUBE_ONLY} = "true" ]]; then
     echo "Running with Minikube, no FsDiff pod created"
 else
-    cat ./local-test-infra/fsdiff-deployment.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
+    cat ./local-test-infra/fsdiff-pod.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
     oc apply -f ./temp/rendered-fsdiff-template.yaml
-    rm ./temp/rendered-fsdiff-template.yaml
+#    rm ./temp/rendered-fsdiff-template.yaml
     sleep 3
     oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=node-master" --timeout=30s
 fi
