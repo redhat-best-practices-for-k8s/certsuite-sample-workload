0a5897663fc0b41ed6beede9967e8127bd4083dd
salah hamadi <shamadi@redhat.com>
2021-08-27 13:43:21 -0400

    remove fsdiff dependency

--- deploy-partner-pods.sh
+++ deploy-partner-pods.sh
@@ -13,17 +13,6 @@ else
     echo "namespace ${NAMESPACE_TO_GENERATE} already exists, no reason to recreate"
 fi
 
-if [[ -n "${TNF_MINIKUBE_ONLY}" ]] && [[ ${TNF_MINIKUBE_ONLY} = "true" ]]; then
-    echo "Running with Minikube, no FsDiff pod created"
-else
-    cat ./local-test-infra/fsdiff-pod.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
-    oc apply -f ./temp/rendered-fsdiff-template.yaml
-#    rm ./temp/rendered-fsdiff-template.yaml
-    sleep 3
-    oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=node-master" --timeout=30s
-fi
-
-
 cat ./local-test-infra/local-partner-deployment.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
 oc apply -f ./temp/rendered-partner-template.yaml
 rm ./temp/rendered-partner-template.yaml
