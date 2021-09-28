e9ae7e4b3d494f1b97641923210ecff9c90e37bf
Jun Chen <jun@redhat.com>
2021-09-28 12:43:23 -0400

    Templatize the image repo url for the partner pod

--- deploy-partner-pods.sh
+++ deploy-partner-pods.sh
@@ -1,8 +1,7 @@
 mkdir -p ./temp
 
-if [[ -z "${TNF_PARTNER_NAMESPACE}" ]]; then
-    export TNF_PARTNER_NAMESPACE="tnf"
-fi
+export TNF_PARTNER_NAMESPACE="${TNF_PARTNER_NAMESPACE:-tnf}"
+export TNF_PARTNER_REPO="${TNF_PARTNER_REPO:-quay.io/testnetworkfunction}"
 
 export res=$(oc get namespace $TNF_PARTNER_NAMESPACE 2>&1)
 if [[ ${res#Error from server (NotFound)} != ${res} ]] || [[ ${res#No resources found} != ${res} ]]; then
@@ -15,6 +14,6 @@ fi
 
 cat ./local-test-infra/local-partner-deployment.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
 oc apply -f ./temp/rendered-partner-template.yaml
-rm ./temp/rendered-partner-template.yaml
+#rm ./temp/rendered-partner-template.yaml
 sleep 3
 oc wait pod -n $TNF_PARTNER_NAMESPACE --for=condition=ready -l "app=partner" --timeout=30s
