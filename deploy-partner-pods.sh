2fda0901c1c55b0deb20abdea88819d0a5b6f4d4
Jun Chen <jun@redhat.com>
2021-09-28 13:03:59 -0400

    Undo temporary changes

--- deploy-partner-pods.sh
+++ deploy-partner-pods.sh
@@ -14,6 +14,6 @@ fi
 
 cat ./local-test-infra/local-partner-deployment.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
 oc apply -f ./temp/rendered-partner-template.yaml
-#rm ./temp/rendered-partner-template.yaml
+rm ./temp/rendered-partner-template.yaml
 sleep 3
 oc wait pod -n $TNF_PARTNER_NAMESPACE --for=condition=ready -l "app=partner" --timeout=30s
