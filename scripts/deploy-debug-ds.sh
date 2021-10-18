0918ef51679c43b5728938cb6ac5dcfdade16ad5
Salaheddine Hamadi <shamadi@redhat.com>
2021-10-18 15:04:43 -0400

    PR comments

--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -3,5 +3,10 @@
 # Initialization
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
-
-oc create -f ./test-partner/debugpartner.yaml
+# check if we're using minikube by looking for kube-apiserver-minikube pod
+# if it's minikube, don't install debug partner
+res=`oc get pod -A | grep -i kube-apiserver-minikube`
+if [[ "$res" -eq "" ]]
+then
+   oc create -f ./test-partner/debugpartner.yaml
+fi
