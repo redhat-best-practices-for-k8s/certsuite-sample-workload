e1bb99d2434ae54a8dc2e7f58f3282b48dedb36a
Salaheddine Hamadi <shamadi@redhat.com>
2021-10-19 09:50:06 -0400

    address David's PR

--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -5,8 +5,10 @@ SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 # check if we're using minikube by looking for kube-apiserver-minikube pod
 # if it's minikube, don't install debug partner
-res=`oc get pod -A | grep -i kube-apiserver-minikube`
-if [[ "$res" -eq "" ]]
+res=`oc get pod -A | grep -io kube-apiserver-minikube`
+if [[ -z "$res" ]]
 then
    oc create -f ./test-partner/debugpartner.yaml
+else
+  echo "minikube detected, skip installing debug daemonSet"
 fi
