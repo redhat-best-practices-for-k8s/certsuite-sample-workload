f74a78c98fa2d69076c46b7645dc040300ac16cc
Salaheddine Hamadi <shamadi@redhat.com>
2021-10-20 21:08:26 -0400

    add template for image registry

--- scripts/delete-debug-ds.sh
+++ scripts/delete-debug-ds.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# Initialization
-SCRIPT_DIR=$(dirname "$0")
-source $SCRIPT_DIR/init-env.sh
-
 oc delete daemonsets.apps/debug -n default
--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -3,12 +3,19 @@
 # Initialization
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
+
+export REDHAT_RHEL_REGISTRY="${REDHAT_RHEL_REGISTRY:-registry.redhat.io/rhel8}"
 # check if we're using minikube by looking for kube-apiserver-minikube pod
 # if it's minikube, don't install debug partner
 res=`oc get pod -A | grep -io kube-apiserver-minikube`
 if [[ -z "$res" ]]
 then
-   oc create -f ./test-partner/debugpartner.yaml
+  echo "use registry $REDHAT_RHEL_REGISTRY"
+  mkdir -p ./temp
+  cat ./test-partner/debugpartner.yaml | $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
+  oc create -f ./temp/debugpartner.yaml
+  rm ./temp/debugpartner.yaml
+  sleep 3
 else
   echo "minikube detected, skip installing debug daemonSet"
 fi
