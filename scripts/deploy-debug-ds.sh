e09c86089604ea89bf635d3803e144b106d24fea
Salaheddine Hamadi <shamadi@redhat.com>
2021-10-26 11:51:42 -0400

    fix PR comments

--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -7,14 +7,14 @@ source $SCRIPT_DIR/init-env.sh
 export REDHAT_RHEL_REGISTRY="${REDHAT_RHEL_REGISTRY:-registry.redhat.io/rhel8}"
 # check if we're using minikube by looking for kube-apiserver-minikube pod
 # if it's minikube, don't install debug partner
-res=`oc get pod -A | grep -io kube-apiserver-minikube`
+res=`oc version | grep  Server`
 if [[ -z "$res" ]]
 then
+  echo "minikube detected, skip installing debug daemonSet"
+else
   echo "use registry $REDHAT_RHEL_REGISTRY"
   mkdir -p ./temp
   cat ./test-partner/debugpartner.yaml | $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
   oc create -f ./temp/debugpartner.yaml
   rm ./temp/debugpartner.yaml
-else
-  echo "minikube detected, skip installing debug daemonSet"
 fi
