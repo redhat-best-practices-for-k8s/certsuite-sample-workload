c04e1bfabadc8f4987d4ce92a76f1f9ede910f98
Salaheddine Hamadi <shamadi@redhat.com>
2021-10-26 11:57:24 -0400

    daemonSet file for debug (#44)
    * daemonSet file for debug
    
    * PR comments
    
    * address David's PR
    
    * add template for image registry
    
    * address comments
    
    * clean comments and update command
    
    * add node selector to debug pods
    
    * fix PR comments

---
+++ scripts/deploy-debug-ds.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Initialization
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/init-env.sh
+
+export REDHAT_RHEL_REGISTRY="${REDHAT_RHEL_REGISTRY:-registry.redhat.io/rhel8}"
+# check if we're using minikube by looking for kube-apiserver-minikube pod
+# if it's minikube, don't install debug partner
+res=`oc version | grep  Server`
+if [[ -z "$res" ]]
+then
+  echo "minikube detected, skip installing debug daemonSet"
+else
+  echo "use registry $REDHAT_RHEL_REGISTRY"
+  mkdir -p ./temp
+  cat ./test-partner/debugpartner.yaml | $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
+  oc create -f ./temp/debugpartner.yaml
+  rm ./temp/debugpartner.yaml
+fi
