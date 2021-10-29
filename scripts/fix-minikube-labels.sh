c352fc32e66abd999aa8db6b429c4a6244886ca2
edcdavid <86730676+edcdavid@users.noreply.github.com>
2021-10-29 14:58:44 -0500

    Fix minikube labels, add initialization (#49)
    * Fix Minikube labels
    
    * use bash to run script in github

--- scripts/fix-minikube-labels.sh
+++ scripts/fix-minikube-labels.sh
@@ -1,5 +1,11 @@
+#!/usr/bin/env bash
+
+# Initialization
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/init-env.sh
+
 res=`oc version | grep  Server`
-if [[ -z "$res" ]]
+if [ -z "$res" ]
 then
   echo "minikube detected, applying worker labels on all nodes"
   oc get nodes -oname | xargs -I{} oc label  {}  node-role.kubernetes.io/worker=worker --overwrite
