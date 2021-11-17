692d7d3e6449a1cc969ca2be1c8a734d8ca13cb9
aabughosh <aabugosh@redhat.com>
2021-11-17 18:55:23 +0200

    deleting the hpa deploymnet

--- scripts/delete-hpa-test-pods.sh
+++ scripts/delete-hpa-test-pods.sh
@@ -5,7 +5,5 @@ SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 
 # Delete the hpa 
-oc delete hpa hpa-test -n ${TNF_EXAMPLE_CNF_NAMESPACE}
-# Delete test deployment
-oc delete  deployment.apps/hpa-test -n ${TNF_EXAMPLE_CNF_NAMESPACE}
+oc delete hpa test -n ${TNF_EXAMPLE_CNF_NAMESPACE}
 
--- scripts/deploy-hpa-test-pod.sh
+++ scripts/deploy-hpa-test-pod.sh
@@ -4,15 +4,5 @@
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 
-# Create namespace if it does not exist
-oc create namespace ${TNF_EXAMPLE_CNF_NAMESPACE} 2>/dev/null
-
-mkdir -p ./temp
-cat ./local-test-infra/local-hpa-pod-under-test.yaml | $SCRIPT_DIR/mo > ./temp/rendered-local-hpa-pod-under-test-template.yaml
-oc apply -f ./temp/rendered-local-hpa-pod-under-test-template.yaml
-rm ./temp/rendered-local-hpa-pod-under-test-template.yaml
-sleep 3
-
-oc wait deployment hpa-test -n $TNF_EXAMPLE_CNF_NAMESPACE --for=condition=available --timeout=$TNF_DEPLOYMENT_TIMEOUT
-
-oc autoscale deployment hpa-test -n $TNF_EXAMPLE_CNF_NAMESPACE --cpu-percent=50 --min=2 --max=3
+# create hpa on the test deployment
+oc autoscale deployment test -n $TNF_EXAMPLE_CNF_NAMESPACE --cpu-percent=50 --min=2 --max=3
