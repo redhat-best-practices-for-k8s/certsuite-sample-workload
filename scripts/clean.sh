57c6511f3d1e387af664de25a30356ee9ab41b6a
aabughosh <aabugosh@redhat.com>
2021-11-17 19:51:01 +0200

    update file names

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,6 +8,6 @@ source $SCRIPT_DIR/init-env.sh
 ./$SCRIPT_DIR/delete-operator.sh
 ./$SCRIPT_DIR/delete-partner-pods.sh
 ./$SCRIPT_DIR/delete-test-pods.sh
-./$SCRIPT_DIR/delete-hpa-test-pods.sh
+./$SCRIPT_DIR/delete-hpa.sh
 ./$SCRIPT_DIR/delete-test-crds.sh
 ./$SCRIPT_DIR/delete-debug-ds.sh
