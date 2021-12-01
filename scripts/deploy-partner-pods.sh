166974325e0ff69d14ce8cfc7e7006d8a35b741e
David Elie-Dit-Cosaque <deliedit@redhat.com>
2021-12-01 12:42:02 -0600

    Wait for pods to be terminated after scale in

--- scripts/deploy-partner-pods.sh
+++ scripts/deploy-partner-pods.sh
@@ -10,13 +10,13 @@ oc apply -f ./temp/rendered-partner-template.yaml
 rm ./temp/rendered-partner-template.yaml
 sleep 3
 
-# adding oc get pods here to refresh the pod list for oc wait, bug ?
-oc get pods
 oc wait deployment tnfpartner -n default --for=condition=available --timeout=$TNF_DEPLOYMENT_TIMEOUT
 
-# Minikube bug needs another scale out?
+# Minikube bug needs a scale in/out to reflect the multus changes, bug?
 oc scale --replicas=0 deployment tnfpartner -n default
+# wait for the pods to be really terminated
+kubectl wait  --for=delete pod --selector=app=tnfpartner --timeout=$TNF_DEPLOYMENT_TIMEOUT
+# sacle out again
 oc scale --replicas=1 deployment tnfpartner -n default
-# adding oc get pods here to refresh the pod list for oc wait, bug ?
-oc get pods
+# wait for the deployment to be available
 oc wait deployment tnfpartner -n default --for=condition=available --timeout=$TNF_DEPLOYMENT_TIMEOUT
