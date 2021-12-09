fce17cbec087447aea439057bfae6eb5c3bb690a
Javier Pena <jpena@redhat.com>
2021-12-09 17:31:54 +0100

    Additional debug partner image fixes
    - Remove unnecessary REDHAT_RHEL_REGISTRY env var
    - In Dockerfile, run yum update before yum install

--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -4,11 +4,10 @@
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 
-export REDHAT_RHEL_REGISTRY="${REDHAT_RHEL_REGISTRY:-quay.io/testnetworkfunction}"
 export SUPPORT_IMAGE="${SUPPORT_IMAGE:-debug-partner:latest}"
 # check if we're using minikube by looking for kube-apiserver-minikube pod
 # if it's minikube, don't install debug partner
-echo "use registry $REDHAT_RHEL_REGISTRY"
+echo "using registry $TNF_PARTNER_REPO"
 mkdir -p ./temp
 cat ./test-partner/debugpartner.yaml | $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
 oc apply -f ./temp/debugpartner.yaml
