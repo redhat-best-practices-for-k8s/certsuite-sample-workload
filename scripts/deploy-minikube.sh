27dbb569e7885cda90b38634b91bf0c13ea0782e
Salaheddine Hamadi <shamadi@redhat.com>
2021-12-23 11:51:46 -0500

    smal fixes

--- scripts/deploy-minikube.sh
+++ scripts/deploy-minikube.sh
@@ -3,5 +3,4 @@ set -x
 minikube delete
 
 # Minikube configures the Calico CNI to deploy multus 
-mini9k9ube.
-minikube start --driver=virtualbox --embed-certs --nodes 3 --cni=calico --feature-gates="LocalStorageCapacityIsolation=false"
+minikube start --driver=docker --embed-certs --nodes 3 --cni=calico --feature-gates="LocalStorageCapacityIsolation=false"
