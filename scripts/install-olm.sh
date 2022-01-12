6821d0b1d7fee8fdcbdf67fe4fb8d209b93e6bda
David Elie-Dit-Cosaque <deliedit@redhat.com>
2022-01-11 23:34:43 -0600

    Fix OLM version

--- scripts/install-olm.sh
+++ scripts/install-olm.sh
@@ -6,7 +6,7 @@ source $SCRIPT_DIR/init-env.sh
 
 # Install OLM
 operator-sdk olm uninstall
-operator-sdk olm install
+operator-sdk olm install --version=v0.18.3
 # Wait for all OLM pods to be ready
 kubectl wait --for=condition=ready pod --all=true -nolm --timeout=$TNF_DEPLOYMENT_TIMEOUT
 sleep 5s
