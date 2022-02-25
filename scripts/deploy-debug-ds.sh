67e2a89ccc18d95dbcc382c781b188c030af6104
edcdavid <86730676+edcdavid@users.noreply.github.com>
2022-02-25 07:33:04 -0600

    Always-on debug pods support (#86)
    * Always-on debug pods support
    
    * linter fix
    
    * doc fix

--- scripts/deploy-debug-ds.sh
+++ scripts/deploy-debug-ds.sh
@@ -8,6 +8,18 @@ export SUPPORT_IMAGE="${SUPPORT_IMAGE:-debug-partner:latest}"
 
 echo "using registry $TNF_PARTNER_REPO"
 mkdir -p ./temp
-cat ./test-partner/debugpartner.yaml | $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
+
+if [ "$ON_DEMAND_DEBUG_PODS" = "false" ];
+then
+  echo "configuring always-on debug pods"
+  NODE_SELECTOR=""
+else
+  echo "Configuring on-demand debug pods"
+  NODE_SELECTOR="
+      nodeSelector:
+        test-network-function.com/node: target"
+fi
+
+cat ./test-partner/debugpartner.yaml | NODE_SELECTOR=$NODE_SELECTOR $SCRIPT_DIR/mo > ./temp/debugpartner.yaml
 oc apply -f ./temp/debugpartner.yaml
 rm ./temp/debugpartner.yaml
