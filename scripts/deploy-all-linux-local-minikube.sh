665f9d8a00fae10f0cd6e5d1a82b903a1727d618
Brandon Palm <bpalm@redhat.com>
2022-03-01 09:04:18 -0600

    Add comments to missing scripts

--- scripts/deploy-all-linux-local-minikube.sh
+++ scripts/deploy-all-linux-local-minikube.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 
 $SCRIPT_DIR/create-local-registry.sh
-$SCRIPT_DIR/deploy-minikube.sh
+$SCRIPT_DIR/deploy-minikube.sh # not implemented
 $SCRIPT_DIR/deploy-test-pods.sh
 $SCRIPT_DIR/deploy-hpa.sh
-$SCRIPT_DIR/deploy-partner-pods.sh
+$SCRIPT_DIR/deploy-partner-pods.sh # not implemented
 $SCRIPT_DIR/create-secret.sh
 $SCRIPT_DIR/install-operator-sdk.sh
 $SCRIPT_DIR/install-olm.sh
