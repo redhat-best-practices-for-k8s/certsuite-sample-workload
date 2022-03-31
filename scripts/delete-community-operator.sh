a79343af0277fb75d833c8f4c568697d15e1a730
edcdavid <86730676+edcdavid@users.noreply.github.com>
2022-03-31 12:00:34 -0500

    Deploy certified operator with kind / github CI (#93)
    * Deploy operator that is both redhat certified and community available with kind / github CI
    
    * Addressing PR comments: upgrading olm and operatorsdk, moving variables
    
    * Addressing PR comments: renaming retry variable and removing unecessary operator delete
    
    * fix indentation

---
+++ scripts/delete-community-operator.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Initialization
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/init-env.sh
+
+# delete CSV
+oc delete csv $COMMUNITY_OPERATOR_NAME -n $TNF_EXAMPLE_CNF_NAMESPACE
+
+# delete operator group
+oc delete operatorgroups test-group -n $TNF_EXAMPLE_CNF_NAMESPACE
+
+# delete subscription
+oc delete subscriptions test-subscription -n $TNF_EXAMPLE_CNF_NAMESPACE
--- scripts/install-olm.sh
+++ scripts/install-olm.sh
@@ -4,9 +4,17 @@
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/init-env.sh
 
+#check if operator-sdk is installed and install it if needed
+if [[ -z "$(which operator-sdk 2>/dev/null)" ]]; then
+  echo "operator-sdk executable cannot be found in the path. Will try to install it."
+  $SCRIPT_DIR/install-operator-sdk.sh
+else
+  echo "operator-sdk was found in the path, no need to install it"
+fi
+
 # Install OLM
 operator-sdk olm uninstall
-operator-sdk olm install --version=v0.18.3
+operator-sdk olm install --version=v0.20.0
 # Wait for all OLM pods to be ready
 kubectl wait --for=condition=ready pod --all=true -nolm --timeout=$TNF_DEPLOYMENT_TIMEOUT
 sleep 5s
