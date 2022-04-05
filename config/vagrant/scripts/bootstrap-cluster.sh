85dbd86eeb17444dd246193f14abb4c8a786eeee
Brandon Palm <bpalm@redhat.com>
2022-04-05 13:35:08 -0500

    Address comments

--- config/vagrant/scripts/bootstrap-cluster.sh
+++ config/vagrant/scripts/bootstrap-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download and install Kubebernetes In Docker (Kind):
-sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.11.1/kind-linux-amd64
+sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.12.0/kind-linux-amd64
 sudo chmod +x /usr/bin/kind
 
 # download openshift client
