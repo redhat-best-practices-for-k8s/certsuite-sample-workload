1db636e71911ef8287f706fbb020b40149a47975
shimritproj <shimritp74@gmail.com>
2022-08-31 18:38:23 +0300

    Added a wait command

--- scripts/install-prometheus-operator.sh
+++ scripts/install-prometheus-operator.sh
@@ -7,4 +7,5 @@ source "$SCRIPT_DIR"/init-env.sh
 git clone https://github.com/prometheus-operator/kube-prometheus.git --depth 1 -b v0.11.0
 cd kube-prometheus   
 oc create -f manifests/setup
+oc wait --for condition=available --timeout=60s --all deployments --all-namespaces
 oc get pods -A | grep prometheus
