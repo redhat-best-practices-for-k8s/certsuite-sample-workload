5536a267331784de9a3c777fba25569f9756916e
shimritproj <shimritp74@gmail.com>
2022-08-31 15:14:13 +0300

    Resolved comments of David R

--- scripts/install-prometheus-operator.sh
+++ scripts/install-prometheus-operator.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-
+set -o errexit
 # Initialization
 SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
-# apply the crds and wait for them
+# Apply the crds and wait for them
 git clone https://github.com/prometheus-operator/kube-prometheus.git --depth 1 -b v0.11.0
 cd kube-prometheus   
 oc create -f manifests/setup
-until oc get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
-oc create -f manifests/
 oc get pods -A | grep prometheus
