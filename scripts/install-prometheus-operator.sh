f3f92aee163ba81f5c1746319c0b5dfc1c48989b
shimritproj <shimritp74@gmail.com>
2022-09-01 21:19:31 +0300

    Fixed issue

--- scripts/install-prometheus-operator.sh
+++ scripts/install-prometheus-operator.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
 # Apply the crds and wait for them
 git clone https://github.com/prometheus-operator/kube-prometheus.git --depth 1 -b v0.11.0
-cd kube-prometheus   
-oc create -f manifests/setup
-oc wait --for condition=available --timeout=60s --all deployments --all-namespacesnow
+cd kube-prometheus
+oc apply --server-side -f manifests/setup
+oc wait --for condition=available --timeout=60s --all deployments --all-namespaces
 oc apply -f manifests/
 oc wait --for condition=available --timeout=60s --all deployments --all-namespaces
 oc get pods -A | grep prometheus
