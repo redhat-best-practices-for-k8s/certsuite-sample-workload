0b160bc251b085b3c30081d62cdcaf805db5c208
shimritproj <shimritp74@gmail.com>
2022-09-01 20:43:42 +0300

    Adding commands in file install-prometheus-operator.sh

--- scripts/install-prometheus-operator.sh
+++ scripts/install-prometheus-operator.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -o errexit
 # Initialization
 SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
@@ -6,5 +7,7 @@ source "$SCRIPT_DIR"/init-env.sh
 git clone https://github.com/prometheus-operator/kube-prometheus.git --depth 1 -b v0.11.0
 cd kube-prometheus   
 oc create -f manifests/setup
+oc wait --for condition=available --timeout=60s --all deployments --all-namespacesnow
+oc apply -f manifests/
 oc wait --for condition=available --timeout=60s --all deployments --all-namespaces
 oc get pods -A | grep prometheus
