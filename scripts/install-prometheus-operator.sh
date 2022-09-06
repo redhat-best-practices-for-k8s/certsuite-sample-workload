1b0a2a84e70f23a4944932d273dc432f3304a1e6
shimritproj <shimritp74@gmail.com>
2022-09-06 20:16:32 +0300

    Updated timeout

--- scripts/install-prometheus-operator.sh
+++ scripts/install-prometheus-operator.sh
@@ -3,7 +3,6 @@ set -o errexit
 # Initialization
 SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
-[ -z "$TNF_DEPLOYMENT_TIMEOUT" ] || TNF_DEPLOYMENT_TIMEOUT=30
 # Apply the crds and wait for them
 git clone https://github.com/prometheus-operator/kube-prometheus.git --depth 1 -b v0.11.0
 cd kube-prometheus
