f95572c642def849c9b60d90a6b38ca92570adfb
David Rabkin <david@rabkin.co.il>
2023-01-12 22:07:07 +0200

    Improve wait mechanizm for hazelcast

--- scripts/deploy-community-operator.sh
+++ scripts/deploy-community-operator.sh
@@ -55,22 +55,18 @@ oc apply --filename ./temp/rendered-local-community-operator-subscription.yaml
 cat ./temp/rendered-local-community-operator-subscription.yaml
 rm ./temp/rendered-local-community-operator-subscription.yaml
 
-
-CSV_CHECK_RETRIES=24 # 240 seconds
-while [[ $(oc get csv -n "$TNF_EXAMPLE_CNF_NAMESPACE" "$COMMUNITY_OPERATOR_NAME" -o go-template="{{.status.phase}}") != "Succeeded" && "$CSV_CHECK_RETRIES" -gt 0 ]]; do
-	echo "waiting for $COMMUNITY_OPERATOR_NAME installation to succeed"
-	sleep 10
-	CSV_CHECK_RETRIES=$(($CSV_CHECK_RETRIES-1))
-	oc get pods -n "$TNF_EXAMPLE_CNF_NAMESPACE"
-	oc get sa -n "$TNF_EXAMPLE_CNF_NAMESPACE"
-	echo $CSV_CHECK_RETRIES
-done
-
-if [ "$CSV_CHECK_RETRIES" -le 0  ]; then
-	echo "timed out waiting for the operator to succeed"
+# Gives some time to the resource to come up.
+sleep 30
+oc wait \
+	--for=jsonpath=\{.status.phase\}=Succeeded \
+	csv \
+	-l operators.coreos.com/hazelcast-platform-operator.tnf \
+	-n tnf \
+	--timeout=240s || {
+	printf >&2 'Timed out waiting for the operator to succeed.\n'
 	oc get csv -n "$TNF_EXAMPLE_CNF_NAMESPACE"
 	exit 1
-fi
+}
 
 oc get csv -n "$TNF_EXAMPLE_CNF_NAMESPACE"
 
