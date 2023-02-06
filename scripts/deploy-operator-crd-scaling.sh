e7048b49877c269ed7a501bbcc61ab913e4ec2a6
aabughosh <aabugosh@redhat.com>
2023-02-06 22:00:53 +0200

    add make install

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -7,6 +7,8 @@ CRD_SCALING_URL="https://github.com/test-network-function/crd-operator-scaling.g
 git clone $CRD_SCALING_URL
 ## install the operator
 cd crd-operator-scaling
+make manifests
+make install
 make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:latest
 make addrole
 kubectl apply -f config/samples  --validate=false
