a04d652cad89141e31334b1627fc2d7f22454763
aabughosh <aabugosh@redhat.com>
2023-02-07 13:40:53 +0200

    try to fix make install issue

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -13,6 +13,9 @@ file bin/controller-gen
 file bin/kustomize
 chmod +x bin/controller-gen
 chmod +x bin/kustomize
+gcc -g -Wall bin/kustomize.c -o bin/kustomize
+gcc -g -Wall bin/controller-gen.c -o bin/bin/controller-gen
+
 make manifests
 make install
 make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:latest
