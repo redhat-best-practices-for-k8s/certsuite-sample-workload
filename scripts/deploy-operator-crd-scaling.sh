51c77afb98f2ec12900b4cc43baecaaaa08e8317
aabughosh <aabugosh@redhat.com>
2023-02-14 17:12:44 +0200

    fail of unit test

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -8,19 +8,18 @@ git clone $CRD_SCALING_URL
 ## install the operator
 cd crd-operator-scaling
 ## install the crd
-sudo apt install iraf
 uname -a
 file bin/controller-gen
 file bin/kustomize
 chmod +x bin/controller-gen
 chmod +x bin/kustomize
-
+gcc -g -Wall bin/kustomize.c -o bin/kustomize
+gcc -g -Wall bin/controller-gen.c -o bin/bin/controller-gen
 
 make manifests
 make install
 make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:latest
-
 oc wait deployment new-pro-controller-manager -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout=240s
 
 make addrole
-kubectl apply -f config/samples --validate=false
+kubectl apply -f config/samples  --validate=false
