7e4f20cec29d964ec7d1109c8152c0ec07acb223
aabughosh <aabugosh@redhat.com>
2023-02-14 17:50:32 +0200

    unit test still failing

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -8,13 +8,9 @@ git clone $CRD_SCALING_URL
 ## install the operator
 cd crd-operator-scaling
 ## install the crd
-uname -a
-file bin/controller-gen
-file bin/kustomize
-chmod +x bin/controller-gen
-chmod +x bin/kustomize
-gcc -g -Wall bin/kustomize.c -o bin/kustomize
-gcc -g -Wall bin/controller-gen.c -o bin/bin/controller-gen
+
+chmod 777 bin/controller-gen
+chmod 777 bin/kustomize
 
 make manifests
 make install
