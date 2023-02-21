c28926f02d11f73ecb5caea4386a12ece1a8115c
aabughosh <88486034+aabughosh@users.noreply.github.com>
2023-02-21 17:38:19 +0200

    Crd scaling test (#210)
    * add the file to deploy the operator of the crd
    
    * add the file to deploy the operator of the crd
    
    * add the file to deploy the operator of the crd
    
    * upadte the make file install
    
    * add creating the crs
    
    * add crd scsling to install
    
    * add crd scsling to install
    
    * add make install
    
    * try to fix make install issue
    
    * try to fix make install issue
    
    * try to fix make install issue
    
    * try to fix make install issue
    
    * try to fix make install issue
    
    * try to fix make install issue
    
    * add wait
    
    * add wait
    
    * add wait
    
    * add wait
    
    * remove extra space
    
    * fail of unit test
    
    * fail of unit test
    
    * fail of unit test
    
    * fail of unit test
    
    * unit test still failing
    
    * fix git issue
    
    * remove dir before cloning
    
    * Add new line to the end of the file
    
    * Fix quotation
    
    ---------
    
    Co-authored-by: David Rabkin <david@rabkin.co.il>

---
+++ scripts/deploy-operator-crd-scaling.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_DIR="$(dirname "$0")"
+source "$SCRIPT_DIR"/init-env.sh
+CRD_SCALING_URL=https://github.com/test-network-function/crd-operator-scaling.git
+rm -rf crd-operator-scaling
+git clone $CRD_SCALING_URL
+## install the operator
+cd crd-operator-scaling
+## install the crd
+
+
+make manifests
+make install
+make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:latest
+oc wait deployment new-pro-controller-manager -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout=240s
+
+make addrole
+kubectl apply -f config/samples --validate=false
