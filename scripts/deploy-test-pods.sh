f22eaee62f3bb4f4684c6b73fc575be59be9ad4d
bnshr <bmandal@redhat.com>
2023-03-01 18:21:02 +0100

    Addressed review comments

--- scripts/deploy-test-pods.sh
+++ scripts/deploy-test-pods.sh
@@ -10,16 +10,7 @@ mkdir -p ./temp
 
 # shellcheck disable=SC2002 # Useless cat.
 cat ./test-target/local-pod-under-test.yaml | APP="testdp" RESOURCE_TYPE="Deployment" MULTUS_ANNOTATION=$MULTUS_ANNOTATION "$SCRIPT_DIR"/mo > ./temp/rendered-local-pod-under-test-template.yaml
-
-cp ./test-target/cpu-exclusive-pool-rt-sched-policy-pod.yaml /temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
-cp ./test-target/cpu-shared-pool-non-rt-sched-policy-pod.yaml /temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
-
 oc apply --filename ./temp/rendered-local-pod-under-test-template.yaml
-oc apply --filename ./temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
-oc apply --filename ./temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
-
 rm ./temp/rendered-local-pod-under-test-template.yaml
-rm ./temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
-rm ./temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
 
 oc wait deployment test -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout="$TNF_DEPLOYMENT_TIMEOUT"
