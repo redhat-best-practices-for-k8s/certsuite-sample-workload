04e1e801fdff182e46596dee388723ed62ebedd0
Banashri Mandal <bmandal@redhat.com>
2023-03-02 16:42:51 +0100

    Test Pod for cpu shared + exclusive apps (#205)
    * Add a pod for testing cpu shared with non-rt scheduling policy
    Add a pod for testing cpu exclusive with rt scheduling policy
    
    * Add a pod for testing cpu shared with non-rt scheduling policy
    Add a pod for testing cpu exclusive with rt scheduling policy
    
    * Addressed review comments

---
+++ scripts/deploy-cpu-scheduling-test-pods.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Initialization
+SCRIPT_DIR=$(dirname "$0")
+
+# shellcheck disable=SC1091 # Not following.
+source "$SCRIPT_DIR"/init-env.sh
+
+mkdir -p ./temp
+
+# shellcheck disable=SC2002 # Useless cat.
+cat ./test-target/cpu-exclusive-pool-rt-sched-policy-pod.yaml | "$SCRIPT_DIR"/mo > ./temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
+# shellcheck disable=SC2002 # Useless cat.
+cat ./test-target/cpu-shared-pool-non-rt-sched-policy-pod.yaml | "$SCRIPT_DIR"/mo > ./temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
+
+oc apply --filename ./temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
+oc apply --filename ./temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
+
+rm ./temp/rendered-cpu-exclusive-pool-rt-sched-policy-pod.yaml
+rm ./temp/rendered-cpu-shared-pool-non-rt-sched-policy-pod.yaml
+
+oc wait deployment test -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout="$TNF_DEPLOYMENT_TIMEOUT"
