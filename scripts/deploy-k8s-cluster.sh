b0aecebeec6fc883be301916841f9c2be8b33e07
David Rabkin <david@rabkin.co.il>
2023-03-09 15:26:27 +0200

    Simplify calico yaml downloading

--- scripts/deploy-k8s-cluster.sh
+++ scripts/deploy-k8s-cluster.sh
@@ -5,10 +5,9 @@ kind delete cluster
 # Kind base with kindnetcni and ipv4/ipv6
 kind create cluster --config=config/k8s-cluster/config.yaml
 
-# download the calico YAML and change the image source to quay
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -o temp-calico.yaml
-sed -i 's/docker.io/quay.io/g' temp-calico.yaml
+# Download the calico YAML and change the image source to quay
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml |
+	sed s/docker.io/quay.io/g >temp-calico.yaml
 
-# deploy calico (not needed but more feature rich - for future use)
+# Deploy calico (not needed but more feature rich - for future use)
 oc create -f temp-calico.yaml
-
