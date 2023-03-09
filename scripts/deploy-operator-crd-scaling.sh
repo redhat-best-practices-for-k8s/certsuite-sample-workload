bba269bbc53e90373b0f0700e811da470d594844
aabughosh <88486034+aabughosh@users.noreply.github.com>
2023-03-09 16:23:32 +0200

    need to sleep before the waiting command for the crd to be created (#224)
    * need to sleep before the waiting command for the crd to be created
    
    * add busy wait fot the deployment to appear
    
    * add install crd for test
    
    * delete the deployment installation just to test
    
    * delete the deployment installation just to test
    
    * back the test pods
    
    * eq 4 on test
    
    * eq 2 on test
    
    * update the wait function on the crd
    
    * update the i in eait
    
    * update the i in wait
    
    * update the i in wait
    
    * Fix shellcheck warning
    
    * Fix shellcheck warning #2
    
    * Fix indentation
    
    ---------
    
    Co-authored-by: David Rabkin <david@rabkin.co.il>

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 SCRIPT_DIR="$(dirname "$0")"
 
 # shellcheck disable=SC1091 # Not following.
@@ -11,12 +10,24 @@ git clone $CRD_SCALING_URL
 ## install the operator
 cd crd-operator-scaling || exit 1
 ## install the crd
-
 make manifests
 make install
 make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:latest
 oc wait deployment new-pro-controller-manager -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout=240s
-
 make addrole
 kubectl apply -f config/samples --validate=false
-oc wait deployment jack -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout=240s
+BIT=5
+NUM=15
+for i in $(seq $NUM); do
+	printf 'Wait %d seconds for %d times...\n' $BIT "$i"
+	sleep $BIT
+	kubectl get deployment jack -n "$TNF_EXAMPLE_CNF_NAMESPACE" ||
+		continue
+	oc wait deployment jack \
+		--for=condition=available \
+		--namespace "$TNF_EXAMPLE_CNF_NAMESPACE" \
+		--timeout=240s
+	exit
+done
+printf >&2 'Exit by timeout after %d seconds.\n' $((BIT * NUM))
+exit 1
