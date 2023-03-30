30c5a9695b0767e20b61cc38dacf3ebed84ac69a
Brandon Palm <bpalm@redhat.com>
2023-03-30 14:50:21 -0500

    Remove call to missing script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,7 +7,6 @@ SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
 
 # Delete tnf, partner and operator and litmus
-./"$SCRIPT_DIR"/delete-partner-pods.sh
 ./"$SCRIPT_DIR"/delete-test-pods.sh
 ./"$SCRIPT_DIR"/delete-hpa.sh
 ./"$SCRIPT_DIR"/delete-test-crds.sh
