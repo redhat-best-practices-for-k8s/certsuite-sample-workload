a0445d100e74e1f4e187702c6ae855b4909075bc
Brandon Palm <bpalm@redhat.com>
2023-04-25 07:37:11 -0500

    Add ubuntu-specific option for bootstrapping

---
+++ config/vagrant/scripts/bootstrap-docker-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# update docker config
+sudo bash -c 'cat <<- EOF > /etc/docker/daemon.json
+{
+  "ipv6": true,
+  "fixed-cidr-v6": "2001:db8:1::/64"
+}
+EOF'
+
+# restart docker
+sudo systemctl restart docker
+
+# increase file system limits
+sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl fs.inotify.max_user_instances=512
