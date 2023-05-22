70ce4c5f7c2b3a54439fe45e0d21c8288c037e49
Brandon Palm <bpalm@redhat.com>
2023-05-22 10:08:57 -0500

    Update Kind to v0.20.0

--- config/vagrant/scripts/bootstrap-cluster.sh
+++ config/vagrant/scripts/bootstrap-cluster.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2207
 
 # Download and install Kubernetes In Docker (Kind):
-sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.18.0/kind-linux-amd64
+sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.20.0/kind-linux-amd64
 sudo chmod +x /usr/bin/kind
 
 # download the latest openshift client at a certain release level
