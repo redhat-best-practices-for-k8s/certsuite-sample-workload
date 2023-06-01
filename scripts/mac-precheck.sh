0eea265bbc371e9e7bdcca132c1d590ac92f24b6
David Rabkin <david@rabkin.co.il>
2023-06-01 22:18:24 +0300

    Improve code readibility, no change for logic

--- scripts/mac-precheck.sh
+++ scripts/mac-precheck.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
+# A script to check if the necessary requirements for running the KinD partner
+# cluster on Mac actually exist.
 
-# A script to check if the necessary requirements for running the KinD partner cluster on Mac actually exist.
-
-# Check if Docker is installed
-CHECK_PODMAN=false
-if ! [ -x "$(command -v docker)" ]; then
-
-	# Check if podman is installed in a subsequent step
-	if ! [ -x "$(command -v podman)" ]; then
-		CHECK_PODMAN=true
-    printf "Podman exists\n"
-	fi
-
-  # Default to recommend docker if podman is not installed
-	if [ "$CHECK_PODMAN" == "false" ]; then
-		printf "Docker is not installed. Please install Docker Desktop for Mac and try again.\n"
-		exit 1
-	fi
-else 
-  printf "Docker exists\n" 
-fi
-
-# Check if KIND_EXPERIMENTAL_PROVIDER is set in the environment
-if [ "$CHECK_PODMAN" == "true" ]; then
-	if [ -z "$KIND_EXPERIMENTAL_PROVIDER" ]; then
-		printf "KIND_EXPERIMENTAL_PROVIDER is not set. Please set KIND_EXPERIMENTAL_PROVIDER to podman and try again.\n"
-		exit 1
-	fi
-fi
-
+# Checks whether all commands exits. Loops over the arguments, each one is a
+# command name.
 cmd_exists() {
 	local arg ret=0
 	for arg; do
@@ -42,14 +17,37 @@ cmd_exists() {
 	return $ret
 }
 
+# Checks if docker is installed, then checks if podman is installed.
+CHECK_PODMAN=false
+if ! cmd_exists docker; then
+	if cmd_exists podman; then
+		CHECK_PODMAN=true
+		printf 'Podman exists.\n'
+	fi
+
+	# Default to recommend docker if podman is not installed.
+	if [ "$CHECK_PODMAN" = false ]; then
+		printf 'Unable to continue, Docker is not installed.\n'
+		exit 1
+	fi
+else
+	printf 'Docker exists.\n'
+fi
+
+# Check if KIND_EXPERIMENTAL_PROVIDER is set in the environment.
+if [ "$CHECK_PODMAN" = true ]; then
+	if [ -z "$KIND_EXPERIMENTAL_PROVIDER" ]; then
+		printf "KIND_EXPERIMENTAL_PROVIDER is not set. Please set KIND_EXPERIMENTAL_PROVIDER to podman and try again.\n"
+		exit 1
+	fi
+fi
 cmd_exists \
+	go \
+	gsed \
 	jq \
 	kind \
 	kubectl \
 	oc \
-  sed \
-  gsed \
-  operator-sdk \
-  go ||
+	operator-sdk \
+	sed ||
 	exit 1
-
