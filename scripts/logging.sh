a26880d28567abe084a757cb3d804c5529de29cb
Gonzalo Reyero Ferreras <87083379+greyerof@users.noreply.github.com>
2023-06-01 17:42:41 +0200

    Install crd-scaling-operator using the corresponding container version. (#283)
    * Install crd-scaling-operator using the corresponding container version.
    
    The script deploy-operator-crd-scaling.sh has been changed to deploy the
    operator using the manager pod image that matches the configured crd
    operator version, currently: v0.0.4 (as per CRD_SCALING_TAG env var
    defined in scripts/init-env.sh).
    
    Also, some not needed commands have been removed, like "make install" or
    "make manifests", and some parts have been refactored, adding relevant
    comments on each step.
    
    * Added printf-based logging helper functions.
    
    * Added new-line at the end of scripts/logging.sh
    
    * Updated logging funcs using/inspired by David R. suggestions.
    
    * Changed spaces to tab in loggin.sh.

---
+++ scripts/logging.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Helper functions for console (stdout/stderr) logging.
+#
+
+COLOR_GREEN="\033[0;32m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_RED="\033[0;31m"
+COLOR_RESET="\033[0m"
+
+# Helper function, not to use directly.
+# Emits the current timestamp to stdout to be used as header in logging traces.
+# If `date` command fails, it will print the error output instead and will
+# exit with status code 1.
+timestamp() {
+	local tms
+	tms="$(date +%Y%m%d-%H:%M:%S 2>&1)" || {
+		printf >&2 "%s\n" "Failed to run date command: $tms"
+		exit 1
+	}
+	printf %s "$tms"
+}
+
+# Info logger: emits arguments to stdout with date-time and "INFO" as header.
+# Example:
+#  20230531-02:41:52 INFO : test for tracing with log_info
+log_info() {
+	local tms
+	tms="$(timestamp)" || exit $?
+	printf "${COLOR_GREEN}%s INFO : ${COLOR_RESET}%s\n" "$tms" "$*"
+}
+
+# Warning logger: emits arguments to stderr with date-time and "WARN" as header.
+# Example:
+#  20230531-02:41:52 WARN : test for tracing with log_warn
+log_warn() {
+	local tms
+	tms="$(timestamp)" || exit $?
+	printf >&2 "${COLOR_YELLOW}%s WARN : ${COLOR_RESET}%s\n" "$tms" "$*"
+}
+
+# Error logger: emits arguments to stderr with date-time and "ERROR" as header.
+#  20230531-02:41:52 ERROR: test for tracing with log_error
+log_error() {
+	local tms
+	tms="$(timestamp)" || exit $?
+	printf >&2 "${COLOR_RED}%s ERROR: ${COLOR_RESET}%s\n" "$tms" "$*"
+}
