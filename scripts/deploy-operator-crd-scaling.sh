e9fa12568327b8ac10b7a996a9d518b5794bae3e
aabughosh <88486034+aabughosh@users.noreply.github.com>
2023-06-22 21:54:06 +0300

    update the release version of the crd scaling (#297)
    * update the release version of the crd scaling
    
    * add make install
    
    * change the release
    
    * remove the tmux

--- scripts/deploy-operator-crd-scaling.sh
+++ scripts/deploy-operator-crd-scaling.sh
@@ -27,6 +27,8 @@ pushd "${CHECKOUT_FOLDER}" || exit 1
 ## Deploy operator using container matching the checkout version ($CRD_SCALING_TAG).
 log_info "Deploying operator with container image version ${CRD_SCALING_TAG}"
 
+make install
+
 make deploy IMG=quay.io/testnetworkfunction/crd-operator-scaling:"${CRD_SCALING_TAG}"
 oc wait deployment new-pro-controller-manager -n "$TNF_EXAMPLE_CNF_NAMESPACE" --for=condition=available --timeout=240s
 
