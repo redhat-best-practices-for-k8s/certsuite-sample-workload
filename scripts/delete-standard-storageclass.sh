2ef9d91fde8a141951b1179bd07c2e64dd00b899
Brandon Palm <bpalm@redhat.com>
2023-07-31 15:13:59 -0500

    Delete 'standard' storageclass at end of install (#318)

---
+++ scripts/delete-standard-storageclass.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Initialization
+SCRIPT_DIR=$(dirname "$0")
+
+# shellcheck disable=SC1091 # Not following.
+source "$SCRIPT_DIR"/init-env.sh
+
+oc delete storageclass standard --ignore-not-found
