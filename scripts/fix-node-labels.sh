3cde1447fc9cc90dff870b934a2cb802685aa20f
Brandon Palm <bpalm@redhat.com>
2023-08-10 11:17:57 -0500

    Add 'worker-cnf' labels to workers

--- scripts/fix-node-labels.sh
+++ scripts/fix-node-labels.sh
@@ -8,4 +8,5 @@ source "$SCRIPT_DIR"/init-env.sh
 if $TNF_NON_OCP_CLUSTER; then
 	echo "non ocp cluster detected, applying worker labels on all nodes"
 	oc get nodes -oname | xargs -I{} oc label {} node-role.kubernetes.io/worker=worker --overwrite
+	oc get nodes -oname | xargs -I{} oc label {} node-role.kubernetes.io/worker-cnf= --overwrite
 fi
