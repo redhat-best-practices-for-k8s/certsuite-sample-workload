1bf0f6c3e4ef8918749e4c96b39c7482eed6dd03
Brandon Palm <bpalm@redhat.com>
2024-01-16 10:05:15 -0600

    Update Istio to v1.20.2

--- scripts/delete-istio.sh
+++ scripts/delete-istio.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(dirname "$0")
 
 # shellcheck disable=SC1091 # Not following.
 source "$SCRIPT_DIR"/init-env.sh
-ISTIO_DIR=istio-1.20.0
+ISTIO_DIR=istio-1.20.2
 if [ ! -d "$ISTIO_DIR" ]; then
-	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.0 sh -
+	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.2 sh -
 fi
 ./$ISTIO_DIR/bin/istioctl uninstall -y --purge
 oc label namespace "$TNF_EXAMPLE_CNF_NAMESPACE" istio-injection-
