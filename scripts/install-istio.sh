cfafcc9c14ce5f865b52ab11acf76f021e1d20f4
Brandon Palm <bpalm@redhat.com>
2024-01-16 10:35:53 -0600

    Update Istio to v1.20.2 (#387)

--- scripts/install-istio.sh
+++ scripts/install-istio.sh
@@ -6,11 +6,11 @@ SCRIPT_DIR=$(dirname "$0")
 # shellcheck disable=SC1091 # File not following.
 source "$SCRIPT_DIR"/init-env.sh
 
-ISTIO_DIR=istio-1.20.0
+ISTIO_DIR=istio-1.20.2
 ISTIO_PROFILE=demo
 
 if [ ! -d "$ISTIO_DIR" ]; then
-	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.0 sh -
+	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.2 sh -
 fi
 oc create namespace istio-system
 if ! $TNF_NON_OCP_CLUSTER; then
