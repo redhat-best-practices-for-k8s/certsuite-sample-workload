9794216280c80ba83c0b519a30c13aa3b3688e93
jmontesi <jmontesi@redhat.com>
2024-04-10 11:36:33 +0200

    Add a role to handle the resources created by the test CRD
    This allows the test "access-control-crd-roles" to run.

--- scripts/delete-test-crds.sh
+++ scripts/delete-test-crds.sh
@@ -6,5 +6,8 @@ SCRIPT_DIR=$(dirname "$0")
 # shellcheck disable=SC1091 # Not following.
 source "$SCRIPT_DIR"/init-env.sh
 
-# Delete test deployment
+# Delete role
+oc delete role crdexample-role -n "${TNF_EXAMPLE_CNF_NAMESPACE}" --ignore-not-found=true
+
+# Delete CRD
 oc delete crd crdexamples.test-network-function.com --ignore-not-found=true
