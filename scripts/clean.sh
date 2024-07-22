eceef6bb03f9722135926ae276affc010fb71a94
Brandon Palm <bpalm@redhat.com>
2024-07-22 13:49:58 -0500

    Remove litmus related items

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,14 +6,13 @@ SCRIPT_DIR=$(dirname "$0")
 # shellcheck disable=SC1091 # Not following.
 source "$SCRIPT_DIR"/init-env.sh
 
-# Delete tnf, partner and operator and litmus
+# Delete tnf, partner and operator
 ./"$SCRIPT_DIR"/delete-test-pods.sh
 ./"$SCRIPT_DIR"/delete-special-resources.sh
 ./"$SCRIPT_DIR"/delete-hpa.sh
 ./"$SCRIPT_DIR"/delete-test-crds.sh
 ./"$SCRIPT_DIR"/delete-statefulset-pods.sh
 ./"$SCRIPT_DIR"/delete-community-operator.sh
-./"$SCRIPT_DIR"/delete-litmus-operator.sh
 ./"$SCRIPT_DIR"/delete-resource-quota.sh
 ./"$SCRIPT_DIR"/delete-pod-disruption-budget.sh
 ./"$SCRIPT_DIR"/delete-cr-scale-operator.sh
