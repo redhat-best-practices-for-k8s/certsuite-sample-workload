427e1bbfbd6309c6fdf398a7d3646674066a3d37
Brandon Palm <bpalm@redhat.com>
2024-09-04 13:28:43 -0500

    Update KinD to v0.24.0 (#485)

--- config/vagrant/scripts/bootstrap-cluster.sh
+++ config/vagrant/scripts/bootstrap-cluster.sh
@@ -2,11 +2,11 @@
 # shellcheck disable=SC2207
 
 # Download and install Kubernetes In Docker (Kind):
-sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.23.0/kind-linux-amd64
+sudo curl -Lo /usr/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.24.0/kind-linux-amd64
 sudo chmod +x /usr/bin/kind
 
 # download the latest openshift client at a certain release level
-RELEASE_LEVEL="4.12"
+RELEASE_LEVEL="4.16"
 VERSIONS=($(sudo curl -sH 'Accept: application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=stable-${RELEASE_LEVEL}&arch=amd64" | jq -r '.nodes[].version' | sort))
 IFS=$'\n' SORTED_VERSIONS=($(sort -V <<<"${VERSIONS[*]}")); unset IFS
 OPENSHIFT_VERSION=${SORTED_VERSIONS[${#SORTED_VERSIONS[@]} - 1]}
