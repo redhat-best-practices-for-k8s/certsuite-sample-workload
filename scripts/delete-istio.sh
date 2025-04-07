cf3592571de1ca00ee327f9a45d8e90388eb6ff6
Brandon Palm <bpalm@redhat.com>
2025-04-07 14:27:57 -0500

    Update Go to v1.24.2

--- scripts/delete-istio.sh
+++ scripts/delete-istio.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(dirname "$0")
 
 # shellcheck disable=SC1091 # Not following.
 source "$SCRIPT_DIR"/init-env.sh
-ISTIO_DIR=istio-1.24.1
+ISTIO_DIR=istio-1.24.2
 if [ ! -d "$ISTIO_DIR" ]; then
-	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.24.1 sh -
+	curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.24.2 sh -
 fi
 ./$ISTIO_DIR/bin/istioctl uninstall -y --purge
 oc label namespace "$CERTSUITE_EXAMPLE_NAMESPACE" istio-injection-
