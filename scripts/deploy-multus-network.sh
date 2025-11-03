51cf7ecdf437c8b23f89f4694689fb96b6ebb100
Brandon Palm <bpalm@redhat.com>
2025-11-03 09:22:54 -0600

    Update Multus CNI to v4.2.3 (#612)
    Co-authored-by: sebrandon1 <4563082+sebrandon1@users.noreply.github.com>

--- scripts/deploy-multus-network.sh
+++ scripts/deploy-multus-network.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR"/init-env.sh
 
 MULTUS_GIT_URL="https://github.com/k8snetworkplumbingwg/multus-cni.git"
-MULTUS_VERSION="v4.2.2"
+MULTUS_VERSION="v4.2.3"
 WHEREABOUTS_GIT_URL="https://github.com/k8snetworkplumbingwg/whereabouts"
 WHEREABOUTS_VERSION="v0.9.2"
 
